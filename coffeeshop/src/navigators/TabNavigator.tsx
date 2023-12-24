789be3fe519009df1f54d80b4bf0357a333e51ab
Kayode Oluwasegun <segunkayode00@gmail.com>
2023-12-24 12:22:46 +0100

    feat: navigation bar

--- coffeeshop/src/navigators/TabNavigator.tsx
+++ coffeeshop/src/navigators/TabNavigator.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import HomeScreen from '../screens/HomeScreen';
 import FavouritesScreen from '../screens/FavouritesScreen';
