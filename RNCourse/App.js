e0964f7e521bf6e92adafb6332c1d9a792437185
Kayode Oluwasegun <segunkayode00@gmail.com>
2024-01-03 23:56:02 +0100

    rncourse: demo todo app

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { useState, useRef } from "react";
 import {
   Button,
   FlatList,
+  Modal,
+  Pressable,
   StyleSheet,
   Text,
   TextInput,
@@ -11,15 +14,35 @@ import {
 export default function App() {
   const [text, setText] = useState("");
   const [goals, setGoals] = useState([]);
+  const [idToDelete, setIdToDelete] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const textInputRef = useRef();
 
   const handleClick = () => {
-    goals.push(goals);
+    setGoals((prev) => [
+      ...prev,
+      {
+        id: Math.random().toString(),
+        value: text,
+      },
+    ]);
+    if (textInputRef.current) {
+      textInputRef.current.clear();
+    }
+  };
+
+  const handleDelete = (id) => {
+    const newGoals = goals.filter((goal) => goal.id !== idToDelete);
+    setGoals(newGoals);
+    setIdToDelete(null);
   };
 
   return (
     <View style={styles.appContainer}>
+      <StatusBar style="auto" backgroundColor="green" />
       <View style={styles.inputContainer}>
         <TextInput
+          ref={textInputRef}
           value={text}
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -28,16 +51,55 @@ export default function App() {
         <Button title="Add Goals" onPress={handleClick} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals ...</Text>
+        <FlatList
+          alwaysBounceVertical={false}
+          data={goals}
+          keyExtractor={(item) => item.id}
+          renderItem={(item) => (
+            <View style={styles.goalItem}>
+              <Pressable
+                onPress={() => {
+                  setModalOpen(true);
+                  setIdToDelete(item.item.id);
+                }}
+                android_ripple={{
+                  color: "darkgreen",
+                }}
+              >
+                <Text style={styles.goalText}>{item.item.value}</Text>
+              </Pressable>
+            </View>
+          )}
+        />
       </View>
-      {/* <FlatList renderItem={item => ()} /> */}
+      <Modal visible={modalOpen} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text>Are you sure you want to delete goal</Text>
+          <View style={styles.btnContainer}>
+            <Button
+              title="Cancel"
+              color="black"
+              onPress={() => setModalOpen(false)}
+            />
+            <Button
+              title="Delete"
+              color="red"
+              onPress={() => {
+                handleDelete();
+                setModalOpen(false);
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 16,
+    paddingHorizontal: 16,
+    paddingTop: 50,
     flex: 1,
   },
   inputContainer: {
@@ -58,5 +120,24 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    backgroundColor: "green",
+    marginVertical: 5,
+  },
+  goalText: {
+    color: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "green",
+  },
+  btnContainer: {
+    flexDirection: "row",
+    gap: 10,
   },
 });
