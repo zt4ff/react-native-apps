406420c1b795cf311e8f005a3dd40b3f66ac0601
Kayode Oluwasegun <segunkayode00@gmail.com>
2024-01-03 19:24:46 +0100

    rncourse: setup vscode settings

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const handleClick = () => {
+    goals.push(goals);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={text}
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={setText}
+        />
+        <Button title="Add Goals" onPress={handleClick} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals ...</Text>
+      </View>
+      {/* <FlatList renderItem={item => ()} /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    padding: 16,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
